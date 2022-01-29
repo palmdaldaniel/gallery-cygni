d935093e70b561fcbba7ec2948df9a19f1088549
Daniel Palmdal <daniel.palmdal@cdon.com>
2022-01-29 14:16:54 +0100

    adding cache system 🔥

--- frontend/index.js
+++ frontend/index.js
@@ -1,32 +1,36 @@
 // get element the user will interact with.
 const form = document.querySelector("#form");
 const input = document.querySelector("#search-input");
-const galleryWrapper = document.querySelector(".gallery-wrapper");
 const wrapperGallery = document.querySelector(".wrapper-gallery");
-const imageGrid = document.querySelector(".wrapper-gallery-grid")
+const imageGrid = document.querySelector(".wrapper-gallery-grid");
 
 const spinner = document.querySelector(".loading");
 const formButton = document.querySelector("button");
 
 const url = "http://localhost:3000/api/v1/search";
 
-let cache;
+let cache = [];
 
 const getData = async (value) => {
   try {
-    formButton.disabled = true
+    formButton.disabled = true;
     spinner.classList.add("show");
 
     let result = await fetch(`${url}?search=${value}`);
     result = await result.json();
 
-    formButton.disabled = false
+    formButton.disabled = false;
     spinner.classList.remove("show");
     buildHtmlOutput(result.data);
-    wrapperGallery.classList.add("spacer")
-    // cache = {
-    //   [`${value}`]: result.data,
-    // };
+    wrapperGallery.classList.add("spacer");
+
+    //  add data to cache
+    if (!checkCache(value)) {
+      console.log(cache);
+      cache.push({
+        [`${value}`]: result.data,
+      });
+    }
   } catch (error) {
     console.log(error);
   }
@@ -48,11 +52,32 @@ const buildHtmlOutput = ({ photo }) => {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  imageGrid.innerHTML = ''
-  wrapperGallery.classList.remove("spacer")
 
+  imageGrid.innerHTML = "";
+  wrapperGallery.classList.remove("spacer");
 
   //console.log('cache', cache );
   const inputText = form.search.value.trim();
+
+  const hasCache = checkCache(inputText);
+
+  if (hasCache) {
+    buildHtmlOutput(hasCache);
+    console.log("has cache");
+    return;
+  }
+
+  console.log("has no cache");
   getData(inputText);
 });
+
+const checkCache = (input) => {
+  let isChached = false;
+  cache.forEach((item) => {
+    if (item.hasOwnProperty(input)) {
+      return (isChached = item[input]);
+    }
+  });
+
+  return isChached;
+};
